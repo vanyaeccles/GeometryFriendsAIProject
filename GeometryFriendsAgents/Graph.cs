3417cb38ada6a99763b06fd75d5f199f8cb03b19
Michael Thomas Stroughair <stroughm@ITSERV.SCSS.TCD.IE>
2016-11-25 13:43:30 +0000

    Initial Commit for the Graph Class - Created using the String type instead of the eventual Node type. Blah Blah Blah

---
+++ GeometryFriendsAgents/Graph.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Drawing;
+using GeometryFriends.AI.Perceptions.Information;
+
+namespace GeometryFriendsAgents
+{
+    class Graph
+    {
+        private List<String> nodeList;
+
+        Graph()
+        {
+            nodeList = new List<String>();
+        }
+
+        Graph(ObstacleRepresentation oI, ObstacleRepresentation coI, CollectibleRepresentation cI, Rectangle rect)
+        {
+            nodeList = new List<String>();
+        }
+
+        void addNode(ref String node)
+        {
+            nodeList.Add(node);
+        }
+
+        List<String> getList()
+        {
+            return nodeList;
+        }
+    }
+}
