ffa35365914743fe9d4a17bab8f680c5f8abcea0
sduignan <sduignan@tcd.ie>
2016-11-25 13:52:53 +0000

    Dummy solver class

---
+++ GeometryFriendsAgents/Solver.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace GeometryFriendsAgents
+{
+    class Solver
+    {
+        private Graph workingGraph;
+        private Graph solutionGraph;
+
+        Solver(Graph newGraph)
+        {
+            workingGraph = newGraph;
+        }
+
+        Graph solve()
+        {
+            solutionGraph = workingGraph;
+            return solutionGraph;
+        }
+    }
+}
