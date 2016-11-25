30557fd95b6c57d83cdd11dd00805ce479f60f79
sduignan <sduignan@tcd.ie>
2016-11-25 13:03:53 +0000

    Think I finally have gitignore working right

---
+++ GeometryFriendsAgents/RectangleAgent.cs
@@ -0,0 +1,207 @@
+﻿using GeometryFriends;
+using GeometryFriends.AI;
+using GeometryFriends.AI.Communication;
+using GeometryFriends.AI.Interfaces;
+using GeometryFriends.AI.Perceptions.Information;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+
+namespace GeometryFriendsAgents
+{
+    /// <summary>
+    /// A rectangle agent implementation for the GeometryFriends game that demonstrates simple random action selection.
+    /// </summary>
+    public class RectangleAgent : AbstractRectangleAgent
+    {
+        //agent implementation specificiation
+        private bool implementedAgent;
+        private string agentName = "RandRect";
+
+        //auxiliary variables for agent action
+        private Moves currentAction;
+        private List<Moves> possibleMoves;
+        private long lastMoveTime;
+        private Random rnd;
+
+        //Sensors Information
+        private CountInformation numbersInfo;
+        private RectangleRepresentation rectangleInfo;
+        private CircleRepresentation circleInfo;
+        private ObstacleRepresentation[] obstaclesInfo;
+        private ObstacleRepresentation[] rectanglePlatformsInfo;
+        private ObstacleRepresentation[] circlePlatformsInfo;
+        private CollectibleRepresentation[] collectiblesInfo;
+
+        private int nCollectiblesLeft;
+
+        private List<AgentMessage> messages;
+
+        //Area of the game screen
+        protected Rectangle area;
+
+        public RectangleAgent()
+        {
+            //Change flag if agent is not to be used
+            implementedAgent = true;
+
+            lastMoveTime = DateTime.Now.Second;
+            currentAction = Moves.NO_ACTION;
+            rnd = new Random();
+
+            //prepare the possible moves  
+            possibleMoves = new List<Moves>();
+            possibleMoves.Add(Moves.MOVE_LEFT);
+            possibleMoves.Add(Moves.MOVE_RIGHT);
+            possibleMoves.Add(Moves.MORPH_UP);
+            possibleMoves.Add(Moves.MORPH_DOWN);
+
+            //messages exchange
+            messages = new List<AgentMessage>();
+        }
+
+        //implements abstract rectangle interface: used to setup the initial information so that the agent has basic knowledge about the level
+        public override void Setup(CountInformation nI, RectangleRepresentation rI, CircleRepresentation cI, ObstacleRepresentation[] oI, ObstacleRepresentation[] rPI, ObstacleRepresentation[] cPI, CollectibleRepresentation[] colI, Rectangle area, double timeLimit)
+        {
+            numbersInfo = nI;
+            nCollectiblesLeft = nI.CollectiblesCount;
+            rectangleInfo = rI;
+            circleInfo = cI;
+            obstaclesInfo = oI;
+            rectanglePlatformsInfo = rPI;
+            circlePlatformsInfo = cPI;
+            collectiblesInfo = colI;
+            this.area = area;
+
+            //send a message to the rectangle informing that the circle setup is complete and show how to pass an attachment: a pen object
+            messages.Add(new AgentMessage("Setup complete, testing to send an object as an attachment.", new Pen(Color.BlanchedAlmond)));
+
+            //DebugSensorsInfo();
+        }
+
+        //implements abstract rectangle interface: registers updates from the agent's sensors that it is up to date with the latest environment information
+        public override void SensorsUpdated(int nC, RectangleRepresentation rI, CircleRepresentation cI, CollectibleRepresentation[] colI)
+        {
+            nCollectiblesLeft = nC;
+
+            rectangleInfo = rI;
+            circleInfo = cI;
+            collectiblesInfo = colI;
+        }
+
+        //implements abstract rectangle interface: signals if the agent is actually implemented or not
+        public override bool ImplementedAgent()
+        {
+            return implementedAgent;
+        }
+
+        //implements abstract rectangle interface: provides the name of the agent to the agents manager in GeometryFriends
+        public override string AgentName()
+        {
+            return agentName;
+        }
+
+        //simple algorithm for choosing a random action for the rectangle agent
+        private void RandomAction()
+        {
+            /*
+             Rectangle Actions
+             MOVE_LEFT = 5
+             MOVE_RIGHT = 6
+             MORPH_UP = 7
+             MORPH_DOWN = 8
+            */
+
+            currentAction = possibleMoves[rnd.Next(possibleMoves.Count)];
+
+            //send a message to the circle agent telling what action it chose
+            messages.Add(new AgentMessage("Going to :" + currentAction));
+        }
+
+        //implements abstract rectangle interface: GeometryFriends agents manager gets the current action intended to be actuated in the enviroment for this agent
+        public override Moves GetAction()
+        {
+            return currentAction;
+        }
+
+        //implements abstract rectangle interface: updates the agent state logic and predictions
+        public override void Update(TimeSpan elapsedGameTime)
+        {
+            if (lastMoveTime == 60)
+                lastMoveTime = 0;
+
+            if ((lastMoveTime) <= (DateTime.Now.Second) && (lastMoveTime < 60))
+            {
+                if (!(DateTime.Now.Second == 59))
+                {
+                    RandomAction();
+                    lastMoveTime = lastMoveTime + 1;
+                    //DebugSensorsInfo();
+                }
+                else
+                    lastMoveTime = 60;
+            }
+        }
+
+        //typically used console debugging used in previous implementations of GeometryFriends
+        protected void DebugSensorsInfo()
+        {
+            Log.LogInformation("Rectangle Aagent - " + numbersInfo.ToString());
+
+            Log.LogInformation("Rectangle Aagent - " + rectangleInfo.ToString());
+
+            Log.LogInformation("Rectangle Aagent - " + circleInfo.ToString());
+
+            foreach (ObstacleRepresentation i in obstaclesInfo)
+            {
+                Log.LogInformation("Rectangle Aagent - " + i.ToString("Obstacle"));
+            }
+
+            foreach (ObstacleRepresentation i in rectanglePlatformsInfo)
+            {
+                Log.LogInformation("Rectangle Aagent - " + i.ToString("Rectangle Platform"));
+            }
+
+            foreach (ObstacleRepresentation i in circlePlatformsInfo)
+            {
+                Log.LogInformation("Rectangle Aagent - " + i.ToString("Circle Platform"));
+            }
+
+            foreach (CollectibleRepresentation i in collectiblesInfo)
+            {
+                Log.LogInformation("Rectangle Aagent - " + i.ToString());
+            }
+        }
+
+        //implements abstract rectangle interface: signals the agent the end of the current level
+        public override void EndGame(int collectiblesCaught, int timeElapsed)
+        {
+            Log.LogInformation("RECTANGLE - Collectibles caught = " + collectiblesCaught + ", Time elapsed - " + timeElapsed);
+        }
+
+        //implememts abstract agent interface: send messages to the circle agent
+        public override List<GeometryFriends.AI.Communication.AgentMessage> GetAgentMessages()
+        {
+            List<AgentMessage> toSent = new List<AgentMessage>(messages);
+            messages.Clear();
+            return toSent;
+        }
+
+        //implememts abstract agent interface: receives messages from the circle agent
+        public override void HandleAgentMessages(List<GeometryFriends.AI.Communication.AgentMessage> newMessages)
+        {
+            foreach (AgentMessage item in newMessages)
+            {
+                Log.LogInformation("Rectangle: received message from circle: " + item.Message);
+                if (item.Attachment != null)
+                {
+                    Log.LogInformation("Received message has attachment: " + item.Attachment.ToString());
+                    if (item.Attachment.GetType() == typeof(Pen))
+                    {
+                        Log.LogInformation("The attachment is a pen, let's see its color: " + ((Pen)item.Attachment).Color.ToString());
+                    }
+                }
+            }
+        }
+    }
+}
