0f458bec8c9723072909c6ec1a4008ec10ebcbe5
ecclesva <ecclesva@tdc.ie>
2016-11-25 13:27:52 +0000

    Adding node class

---
+++ GeometryFriendsAgents/Node.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+
+
+namespace GeometryFriendsAgents
+{
+
+    class Node
+    {
+
+        
+        private int x;
+        private int y;
+
+        Node[] node_pointers; //Array of node objects
+        //C# doesn't like pointers
+         
+
+
+
+        //Constructor
+        public Node(int x_coord, int y_coord)
+        {
+            x = x_coord;
+            y = y_coord;
+
+        }
+
+
+
+
+        //AddNeighbour Method
+        public void AddNeighbour(ref Node neighbour)
+        {
+
+        }
+
+
+    }
+}
